5615d4a8f13ddd0807cf0323275787798a6f326b
Dimitri M <dmitic29@users.noreply.github.com>
2021-10-05 17:21:25 -0400

    Added connection request to Exchange Online

--- DistributionListImport.ps1
+++ DistributionListImport.ps1
@@ -2,7 +2,7 @@
 #This script will not create missing contacts for you, but you are free to modify it so it does if you need it to
 #For best results use my other PS script to export data that you are importing in this script
 #Csv needs to have a DisplayName, PrimarySmtpAddress, and Membership field
-
+Connect-ExchangeOnline
 $pathtoimports = Read-Host "Input full path to csv file with distribution group information to import"
 $ImportedDLs = Import-Csv $pathtoimports
 
