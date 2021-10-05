e3eca997bd7db86e48611e8c10ba72da01e0f1b6
Dimitri M <dmitic29@users.noreply.github.com>
2021-10-05 17:12:18 -0400

    Initial upload

---
+++ DistributionListExport.ps1
@@ -0,0 +1,27 @@
+﻿#Use the interactive login prompt to input credentials of your Exchange/Global admin account
+Connect-ExchangeOnline
+
+$exportfilename = Read-Host "Give full path and name of csv file to export data to (include extension .csv)."
+
+#Fetch all distribution groups from Exchange Online
+$distribuitongroups = Get-DistributionGroup -ResultSize Unlimited #Add filter here if you don't want all distribution groups
+
+#Main array that holds the object data to be exported to csv
+$alldata = @()
+foreach ($distrgr in $distribuitongroups) {
+    $members = @()
+    Get-DistributionGroupMember $distrgr.Name | foreach {
+
+        $members += $_.PrimarySmtpAddress + " "
+    }
+    $alldata += New-Object psobject -Property @{
+        DisplayName = $distrgr.DisplayName
+        PrimarySmtpAddress = $distrgr.PrimarySmtpAddress
+        Membership = [string]$members
+    }
+}
+
+#We are exporting column values for DisplayName, PrimarySmtpAddress, and Membership (list of all distribution group members by email address)
+$alldata.GetEnumerator() | Select-Object -Property DisplayName,PrimarySmtpAddress,Membership | Export-Csv -NoTypeInformation -Path "$exportfilename"
+Write-Host -ForegroundColor Green "Data exported to $exportfilename."
+
---
+++ DistributionListImport.ps1
@@ -0,0 +1,16 @@
+﻿#!!!Important!!! Make sure you already have all the contacts imported to Exchange Online contacts before you start the import
+#This script will not create missing contacts for you, but you are free to modify it so it does if you need it to
+#For best results use my other PS script to export data that you are importing in this script
+#Csv needs to have a DisplayName, PrimarySmtpAddress, and Membership field
+
+$pathtoimports = Read-Host "Input full path to csv file with distribution group information to import"
+$ImportedDLs = Import-Csv $pathtoimports
+
+foreach ($DLrow in $ImportedDLs) {
+    New-DistributionGroup -Name $DLrow.DisplayName -DisplayName $DLrow.DisplayName -PrimarySmtpAddress $DLrow.PrimarySmtpAddress -Type "Distribution"
+    $splitmembers = -split $DLrow.Membership
+    foreach ($member in $splitmembers) {
+        Add-DistributionGroupMember -Identity $DLrow.DisplayName -Member $member
+        
+    }
+}
